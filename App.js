98e08677c31bcd14a7c96e352e77fe2be6bfb9b3
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-08-24 14:43:24 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import{Routes, Route} from 'react-router-dom';
+import { About } from './components/About';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { Home } from './components/Home';
+import {Navbar} from './components/Navbar';
+import { NewProducts } from './components/NewProducts';
+import { OrdnerSummary } from './components/OrdnerSummary';
+import {Products} from './components/Products'
+import { UserDetais } from './components/UserDetais';
+import { MyUsers } from './components/MyUsers';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+   <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='about' element={<About/>}></Route>
+      <Route path='order-summary' element={<OrdnerSummary/>}></Route>
+      <Route path='products' element={<Products/>}> 
+        <Route path='featured' element={<FeaturedProducts/>}></Route>
+        <Route path='new' element={<NewProducts/>}></Route>
+      </Route>
+      <Route path='myusers' element={<MyUsers/>}></Route>
+      <Route path='users/:userId' element={<UserDetais/>}></Route>
+   </Routes>
+   </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {staticRouter} from 'react-router';
+import  express  from "express";
+import {reactDomServer} from 'react-dom/server';
+import filessystem from 'fs';
